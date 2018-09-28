90e6a747ab34da6daa6c454f86bb8cf1f8d85bb6
Glitch (hello-express) <none>
2018-09-28 15:58:16 +0000

    💓💴 Checkpoint ./server.js:1330183/937 ./watch.json:1330183/602 ./package.json:1330183/513

--- server.js
+++ server.js
@@ -1,14 +1,45 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const prefix
 
 
 client.on('ready', () => {
-  console.log('')
-  
-
+  console.log('-------------------------------')
+  console.log('        EPSI Bot Online        ')
+  console.log('-------------------------------')
 })
 
+client.on('message',message =>{
+  let msg = message.content.toUpperCase();
+	let sender=message.author;
+	let args = message.content.slice(prefix.length).trim().split(' ');
+  let cmd = args.shift().toLowerCase();
+	if (message.channel.type==="dm") return;
+  if (!msg.startsWith(prefix)) return;
+	if (message.author.bot) return;
+ try {
+  delete require.cache[require.resolve(`./commands/${cmd}.js`)];
+  let ops = {
+    ownerID: ownerID,
+    active: active
+  }
+  let commandFile = require(`./commands/${cmd}.js`);
+  commandFile.run(client, message, args, ops);
 
+  } catch (e) {
+  console.log(e.stack);
+  }  finally {
+    console.log(`${message.author.tag} a utilisé la commande ${cmd}`);}
+  
+  
+
+
+
+
+
+
+
+})
 
 
 
