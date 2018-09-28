9c4514141d9ab21e095da55241017fb36d68a605
Glitch (hello-express) <none>
2018-09-28 15:47:16 +0000

    💀🚜 Checkpoint ./server.js:1330183/2706

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 
+client.on('ready', () => {
+  console.log('')
+  
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -20,70 +37,14 @@ const client = new Discord.Client()
 client.login(process.env.TOKEN)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var express = require('express');
-var app = express();
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
-});
-app.use(express.static('public'))
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + '/views/index.html')
-})
-
-// Simple in-memory store
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-]
-
+const http = require('http');
+const express = require('express');
+const app = express();
 app.get("/", (request, response) => {
   console.log(Date.now() + " Ping Received");
   response.sendStatus(200);
 });
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", (request, response) => {
-  dreams.push(request.query.dream)
-  response.sendStatus(200)
-})
 app.listen(process.env.PORT);
 setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
-
-// listen for requests :)
