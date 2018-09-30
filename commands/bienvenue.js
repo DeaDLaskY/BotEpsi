aba82bb128da385b6a215756232ee9c6fb02ca5b
Glitch (hello-express) <none>
2018-09-30 08:45:37 +0000

    🏗🎽 Checkpoint ./commands/bienvenue.js:1330183/298

---
+++ commands/bienvenue.js
@@ -0,0 +1,20 @@
+exports.run = async (client, message, args) => {
+  
+  if (client.channels.get != '495190443928059904') return
+  
+  message.channel.send('EPSI').then(msg =>{
+    msg.react('✅')
+    
+  
+  })
+
+
+
+
+
+
+
+
+
+
+}
