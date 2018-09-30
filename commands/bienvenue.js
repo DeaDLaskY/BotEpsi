2cacbcd98e4161415e5db6cd2becf4a8d3a0c64b
Glitch (hello-express) <none>
2018-09-30 09:27:35 +0000

    🏁🧀 Checkpoint ./commands/bienvenue.js:1330183/717

--- commands/bienvenue.js
+++ commands/bienvenue.js
@@ -1,15 +1,15 @@
 exports.run = async (client, message, args) => {
   const channel = message.guild.channels.find("name", "bienvenue");
   
-  
+  client.on('messageReactionAdd', (reaction, user) => {
+    if(reaction.emoji.name === "✅") {
+        console.log(reaction.users);
+      let roleepsi = message.guild.roles.get('495887624565817344')
+      reaction.users.addRole(roleepsi)
+    }
+});
   channel.send('EPSI').then(msg =>{
     msg.react('✅')
-    if (msg.user.react === '✅'){
-      var role = client.guild.role.find('name', 'EPSI')
-      msg.addRole(role)
-    }
-    
-  
   })
 
 
