b23fba31b195aff1f6221e91ecd3018d9a55dfab
Glitch (hello-express) <none>
2018-10-10 09:52:47 +0000

    🦃⛹ Checkpoint ./commands/lolstats.js:1330183/483

--- commands/lolstats.js
+++ commands/lolstats.js
@@ -1,1 +1,11 @@
-exports.js  = async (client, messag
+exports.js  = async (client, message, args) => {
+  const lol = require('lol-js');
+  const lolClient = lol.client({
+    apiKey: 'blahblahblah',
+    cache: lol.redisCache({host: '127.0.0.1', port: 6379})
+}
+
+
+
+
+}
