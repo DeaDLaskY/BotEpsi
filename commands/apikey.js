4e038a261a89c2e5899b0e91f0f8cfe1fd554968
Glitch (hello-express) <none>
2018-10-14 18:52:19 +0000

    🚆🏄 Checkpoint ./commands/apikey.js:1330183/125

--- commands/apikey.js
+++ commands/apikey.js
@@ -1,4 +1,6 @@
 exports.run = (client, message, args) => {
-  if(!args[0]) return message.channel.send("La clé d'AP")
+  if(!args[0]) return message.channel.send("La clé d'API sil te plait")
+  const APIKEY = args[0]
+  return message.channel.send(APIKEY)
 
 }
