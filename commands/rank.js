5bc75b51ed9c9517e6e578cbf260404a8daa8d6b
Glitch (hello-express) <none>
2018-10-14 12:29:08 +0000

    🎮🔦 Checkpoint ./commands/rank.js:1330183/1836

--- commands/rank.js
+++ commands/rank.js
@@ -1,23 +1,7 @@
 exports.run = (client, message, args) =>{
-var lolapi = require('lolapi')('my-api-key', 'euw');
-var summonerName = args[0];
-lolapi.Summoner.getByName(summonerName, function (error, summoner) {
-  if (error) throw error;
-  /* summoner object
-  { wickd:
-    {
-      id: 71500,
-      name: 'Wickd',
-      profileIconId: 613,
-      summonerLevel: 30,
-      revisionDate: 1408199475000
-    }
-  } */
-  var summonerId = summoner[summonerName].id;
-  lolapi.Summoner.getRunes(summonerId, function (error, runes) {
-    if (error) throw error;
-    // do something with runes
-  })
+var lolapi = require('lolapi')('RGAPI-30a720d7-8836-41e1-9bf6-aa5cddb03a96', 'euw');
+  lolapi.Champion.getAll({ freeToPlay: true }, function (error, champion) {
+  // do something with free to play champions
 });
 
 }
