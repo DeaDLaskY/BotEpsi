dad1c4ed72a92fab54b45abb75736f9d67cfee25
Glitch (hello-express) <none>
2018-10-17 08:42:02 +0000

    🐊🌦 Checkpoint ./commands/bantemp.js:1330183/704

--- commands/bantemp.js
+++ commands/bantemp.js
@@ -1,6 +1,13 @@
 exports.run = (client, message, args) => {
-  
-  if(!
+  let myRole = message.guild.roles.get('501312575812796418');
+  console.log(myRole)
+  if(message.member.roles.has(myRole)) {
+  console.log(`Yay, the author of the message has the role!`);
+  message.channel.send("...")
+} else {
+  console.log(`Nope, noppers, nadda.`);
+  message.channel.send('!!')
+}
 
 
 
