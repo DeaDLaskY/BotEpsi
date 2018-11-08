9534d907e50cc7660c3db4064718c1a5812d5099
Glitch (hello-express) <none>
2018-11-08 09:45:16 +0000

    🕶🚜 Checkpoint ./commands/rank.js:1330183/20 ./commands/question.js:1330183/478

--- commands/rank.js
+++ commands/rank.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) =>{
 var lol = require('lol-js');
 var lolClient = lol.client({
     apiKey: 'RGAPI-30a720d7-8836-41e1-9bf6-aa5cddb03a96',
-    cache: lol.redisCache({host: '127.0.0.1', port: 6379})
+    cache: lol.redisCache({host: '192.168.1.2', port: 6379})
 });
 lolClient.getChampionById('na', 53, {champData: ['all']}, function(err, data) {
     console.log("Found ", data.name);
