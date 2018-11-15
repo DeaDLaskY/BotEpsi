cab7afa92d1c4b1b4793c27a419d9e16137286d7
Glitch (hello-express) <none>
2018-11-15 20:38:47 +0000

    🛎🎱 Checkpoint ./commands/hello.js:1330183/528

--- commands/hello.js
+++ commands/hello.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args) =>{
-  message.channel.send('Hello!\n\nJe sui le bot de l\'EPSi, codé en **JavaScript** sous le module DiscordJS qui tourne sous NodeJS.\n\nMes fonctionnalités ne sont pas encore très variées mais je peut effectuer:\n    **Un Ping** pour savoir si je suis online\n    De la musique avec les commandes suivantes:\n  !play {nom de la musique}\nil vous suffit d\'entrer le nom de la musique puis de sélectionner parmis ma query Youtube la musique souhaité (only audio')
+  message.channel.send('@here\n\n\nHello!\n\nJe sui le bot de l\'EPSI, codé en **JavaScript** sous le module DiscordJS qui tourne sous NodeJS.\n\nMes fonctionnalités ne sont pas encore très variées mais je peut effectuer:\n\n\n    **Un Ping** pour savoir si je suis online\n\n    De la musique avec les commandes suivantes:\n  !play {nom de la musique}\nil vous suffit d\'entrer le nom de la musique puis de sélectionner parmis ma query Youtube la musique souhaité (only audio)\n\n**Ne pas oublier de se connecter dans un channel vocal avant d\'invoquer le bot!**\n\nPlusieures musiques peuvent etres ajoutées a la file d\'attente, pour la check: !queue\n\nPour mettre en pause et rejouer la musique: !pause et !resume\n un système de skip est disponible en fonction du nombre de personnes présentes dans le channel: !skip pour voter la prochaine musique')
 
 
 }
