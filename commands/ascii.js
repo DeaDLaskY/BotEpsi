add8ab9f2ed9725eb50122fcf48a56008b518f22
Glitch (hello-express) <none>
2018-11-16 08:06:55 +0000

    ☄️🎬 Checkpoint ./commands/ascii.js:1330183/207

--- commands/ascii.js
+++ commands/ascii.js
@@ -1,6 +1,12 @@
 exports.run = async (client, message, args) => {
   const ascii = require('ascii-art');
-  
+  ascii.font(args.join(' '),'Doom', function(rendered){
+  rendered=rendered.trimRight();
+  message.channel.send(rendered, {
+    code:'md'
+  })
+    
+  });
 
 
 
