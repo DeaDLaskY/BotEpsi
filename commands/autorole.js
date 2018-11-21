9b023ccf3670cd1de2a111e561be7a9cf373bba3
Glitch (hello-express) <none>
2018-11-21 09:12:07 +0000

    🐇🌌 Checkpoint ./commands/autorole.js:1330183/996

--- commands/autorole.js
+++ commands/autorole.js
@@ -1,25 +1,25 @@
 exports.run = async (client, message, args) => {
   
-  const roles = {'B1':'role1',
-               'B1Gr1':'role2',
-               'B1Gr2':'role3',
-               'B2':'role4',
-               'B3':'role5',
-               'B3Gr1':'role6',
-               'B3Gr2':'role7',
-               'I4':'role8',
-               'I4Gr1':'role9',
-               'I4Gr2':'role10',
-               'I5':'role11',
-               'I5Gr1':'role12',
-               'I5Gr2':'role13',
-               'BDE':'role14',
-               'EPSI':'role15',
-               'WIS':'role16'}//
+  const roles = ['B1',
+               'B1Gr1',
+               'B1Gr2',
+               'B2',
+               'B3',
+               'B3Gr1',
+               'B3Gr2',
+               'I4',
+               'I4Gr1',
+               'I4Gr2',
+               'I5',
+               'I5Gr1',
+               'I5Gr2',
+               'BDE',
+               'EPSI',
+               'WIS']
   if(!args[0]) return message.channel.send('spécifie un rôle attribuer')
   if (args[0] = roles){
-    message.channel.send('Tu as le rôle '+args[message.content]+' !')
-    console.log(roles[message.content])
+    message.channel.send('Tu as le rôle '+roles[args[0]]+' !').catch(console.error);
+    console.log(roles.length)
   }
 
 
