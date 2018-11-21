a28fd2bbd27a1299e1eb18af1d4ce45635bb8721
Glitch (hello-express) <none>
2018-11-21 08:39:16 +0000

    🏀💡 Checkpoint ./commands/autorole.js:1330183/622

--- commands/autorole.js
+++ commands/autorole.js
@@ -1,8 +1,16 @@
 exports.run = async (client, message, args) => {
   
-  let roles = ['B1','B1Gr1','B1Gr2','B2','B3','B3Gr1','B3Gr2','I4','I4Gr1','I4Gr2','I5','I5Gr1','I5Gr2','BDE','EPSI','WIS']
-  
-  if (args[0] === roles){
+  let roles = {'B1':'roleB1',
+               B1Gr1',
+               'B1Gr2',
+               'B2',
+               'B3',
+               'B3Gr1',
+               'B3Gr2',
+               'I4','I4Gr1','I4Gr2','I5','I5Gr1','I5Gr2','BDE','EPSI','WIS'}
+  if(!args[0]) return message.channel.send('spécifie un rôle attribuer')
+  if (args[0] = roles){
+    message.channel.send('Tu as le rôle '+args[0]+' !')
   }
 
 
