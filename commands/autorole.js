e46bd500278a0d654ec7515ff3a21e8bfe53ccc2
Glitch (hello-express) <none>
2018-11-21 09:56:07 +0000

    💠😜 Checkpoint ./commands/autorole.js:1330183/182

--- commands/autorole.js
+++ commands/autorole.js
@@ -16,15 +16,16 @@ exports.run = async (client, message, args) => {
         collector.on('collect', message => {
             if (message.content == "Grp1") {
                 message.channel.send("Tu as les roles B1 et Grp1");
-              let roleB1Gr1 = client.guild.roles.find('name','B1Gr1')
-              message.author.addRole(roleB1)
+              let roleB1Gr1 = message.guild.roles.find('name','B1Gr1')
+              message.addRole(roleB1Gr1)
+              console.log(message.author.id)
               
             } else if (message.content == "Grp2") {
                 message.channel.send("Tu as les roles B1 et Grp2");
-              let roleB1Gr2 = client.guild.roles.find('name','B1Gr2')
+              let roleB1Gr2 = message.guild.roles.find('name','B1Gr2')
               message.author.addRole(roleB1Gr2)
             }
-          let roleB1 = client.guild.roles.find('name','B1')
+          let roleB1 = message.guild.roles.find('name','B1')
               message.author.addRole(roleB1)
   }
 )}
