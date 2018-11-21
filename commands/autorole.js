42f8658b978ae72e10aabe1b80c4d98932073833
Glitch (hello-express) <none>
2018-11-21 08:28:12 +0000

    🦄🌌 Checkpoint ./server.js:1330183/24 ./commands/autorole.js:1330183/432

---
+++ commands/autorole.js
@@ -0,0 +1,17 @@
+exports.run = async (client, message, args) => {
+  
+  let roles = ['B1','B1Gr1','B1Gr2','B2','B3','B3Gr1','B3Gr2','I4','I4Gr1','I4Gr2','I5','I5Gr1','I5Gr2','BDE','EPSI','WIS']
+  
+  if (args[0] === roles){
+  }
+
+
+
+
+
+
+
+
+
+
+}
